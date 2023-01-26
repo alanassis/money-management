7a21e2c0e9fc6ce94674a5cb71801b3b42daf083
Alan Assis <alanpenteado.work@gmail.com>
2023-01-25 21:34:59 -0300

    test: parse tsconfig paths to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,8 @@
-export default {
+import { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config = {
 	moduleFileExtensions: ['js', 'json', 'ts'],
 	rootDir: 'src',
 	testRegex: '.*\\.spec\\.ts$',
@@ -8,6 +12,11 @@ export default {
 	collectCoverageFrom: [
 		'**/*.(t|j)s'
 	],
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+		prefix: '<rootDir>/..'
+	}),
 	coverageDirectory: '../coverage',
-	testEnvironment: 'node'
+	testEnvironment: 'node',
 };
+
+export default config;
