b73cd0ceca0005dbf32c394d9df7982f62d56b42
Alan Assis <alanpenteado.work@gmail.com>
2023-02-09 21:40:42 -0300

    style: add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
 	plugins: ['@typescript-eslint/eslint-plugin'],
 	extends: [
 		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
 	],
 	root: true,
 	env: {
@@ -19,22 +20,6 @@ module.exports = {
 		'@typescript-eslint/interface-name-prefix': 'off',
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
+		'@typescript-eslint/no-explicit-any': 'off'
 	},
 };
